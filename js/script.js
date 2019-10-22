165c61bf5d4698db84451b4d993fc7cb507981df
GreyM22 <greimuka@gmail.com>
2019-10-22 19:31:47 +0200

    nav bar

--- js/script.js
+++ js/script.js
@@ -121,7 +121,7 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.the-button').click(function () {
     $('.navbar').toggleClass('bg-dark');
-    $('#navbarResponsive').slideToggle('slow')
+    $('#navbarResponsive').fadeToggle('slow')
     $('body').toggleClass('overflow-hidden');
   });
 });
