549ca73cb3d738fc34dfea16bdc3defb1e9883f0
GreyM22 <greimuka@gmail.com>
2019-10-22 14:17:57 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
-function hoverInsta() {
-    document.getElementById('path1-insta').style.fill = '#f7ce9bf3';
-    document.getElementById('path2-insta').style.fill = '#f7ce9bf3';
-    document.getElementById('path3-insta').style.fill = '#f7ce9bf3';
-}
+// function hoverInsta() {
+//     document.getElementById('path1-insta').style.fill = '#f7ce9bf3';
+//     document.getElementById('path2-insta').style.fill = '#f7ce9bf3';
+//     document.getElementById('path3-insta').style.fill = '#f7ce9bf3';
+// }
 
-function notHoverInsta() {
-    document.getElementById('path1-insta').style.fill = '#ffff';
-    document.getElementById('path2-insta').style.fill = '#ffff';
-    document.getElementById('path3-insta').style.fill = '#ffff';
-}
+// function notHoverInsta() {
+//     document.getElementById('path1-insta').style.fill = '#ffff';
+//     document.getElementById('path2-insta').style.fill = '#ffff';
+//     document.getElementById('path3-insta').style.fill = '#ffff';
+// }
 
-function hoverInstaButtom() {
-    document.getElementById('path1-insta-buttom').style.fill = '#f7ce9bf3';
-    document.getElementById('path2-insta-buttom').style.fill = '#f7ce9bf3';
-    document.getElementById('path3-insta-buttom').style.fill = '#f7ce9bf3';
-}
+// function hoverInstaButtom() {
+//     document.getElementById('path1-insta-buttom').style.fill = '#f7ce9bf3';
+//     document.getElementById('path2-insta-buttom').style.fill = '#f7ce9bf3';
+//     document.getElementById('path3-insta-buttom').style.fill = '#f7ce9bf3';
+// }
 
-function notHoverInstaButtom() {
-    document.getElementById('path1-insta-buttom').style.fill = '#ffff';
-    document.getElementById('path2-insta-buttom').style.fill = '#ffff';
-    document.getElementById('path3-insta-buttom').style.fill = '#ffff';
-}
+// function notHoverInstaButtom() {
+//     document.getElementById('path1-insta-buttom').style.fill = '#ffff';
+//     document.getElementById('path2-insta-buttom').style.fill = '#ffff';
+//     document.getElementById('path3-insta-buttom').style.fill = '#ffff';
+// }
 
-function hoverArrowButton() {
-    document.getElementById('arrow-down').style.fill = '#B38A58';
-}
+// function hoverArrowButton() {
+//     document.getElementById('arrow-down').style.fill = '#B38A58';
+// }
 
-function notHoverArrowButton() {
-    document.getElementById('arrow-down').style.fill = 'none';
-}
+// function notHoverArrowButton() {
+//     document.getElementById('arrow-down').style.fill = 'none';
+// }
 
 function clickRomb(nr) {
     for (let i = 1; i <= 3; i++) {
@@ -41,39 +41,39 @@ function clickRomb(nr) {
     }
 }
 
-function hoverInstaFollowButton() {
-    document.getElementById('insta-follow-button1').style.fill = '#000000';
-    document.getElementById('insta-follow-button2').style.fill = '#000000';
-    document.getElementById('insta-follow-button3').style.fill = '#000000';
+// function hoverInstaFollowButton() {
+//     document.getElementById('insta-follow-button1').style.fill = '#000000';
+//     document.getElementById('insta-follow-button2').style.fill = '#000000';
+//     document.getElementById('insta-follow-button3').style.fill = '#000000';
 
-}
+// }
 
-function notHoverInstaFollowButton() {
-    document.getElementById('insta-follow-button1').style.fill = '#ffff';
-    document.getElementById('insta-follow-button2').style.fill = '#ffff';
-    document.getElementById('insta-follow-button3').style.fill = '#ffff';
+// function notHoverInstaFollowButton() {
+//     document.getElementById('insta-follow-button1').style.fill = '#ffff';
+//     document.getElementById('insta-follow-button2').style.fill = '#ffff';
+//     document.getElementById('insta-follow-button3').style.fill = '#ffff';
 
-}
+// }
 
-function hoverSideArrowButton(side) {
-    document.getElementById('Path-' + side).style.stroke = '#b38a58';
-}
+// function hoverSideArrowButton(side) {
+//     document.getElementById('Path-' + side).style.stroke = '#b38a58';
+// }
 
-function notHoverSideArrowButton(side) {
-    document.getElementById('Path-' + side).style.stroke = '#ffff';
-}
+// function notHoverSideArrowButton(side) {
+//     document.getElementById('Path-' + side).style.stroke = '#ffff';
+// }
 
-function hoverArrowUpButton() {
-    document.getElementById("arrow-up").style.stroke = '#b38a58';
-    document.getElementById("arrow-up1").style.stroke = '#b38a58';
+// function hoverArrowUpButton() {
+//     document.getElementById("arrow-up").style.stroke = '#b38a58';
+//     document.getElementById("arrow-up1").style.stroke = '#b38a58';
 
-}
+// }
 
-function notHoverArrowUpButton() {
-    document.getElementById("arrow-up").style.stroke = '#ffff';
-    document.getElementById("arrow-up1").style.stroke = '#ffff';
+// function notHoverArrowUpButton() {
+//     document.getElementById("arrow-up").style.stroke = '#ffff';
+//     document.getElementById("arrow-up1").style.stroke = '#ffff';
 
-}
+// }
 
 window.addEventListener('scroll', function () {
     let scroll = window.scrollY;
@@ -87,3 +87,34 @@ window.addEventListener('scroll', function () {
     }
 
 });
+
+function moveToFirsBodySection(){
+    document.querySelector('.first-body-section').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+}
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
