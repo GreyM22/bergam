e3d4cbc94f103453fb8307de6e02eb374bf995fb
GreyM22 <greimuka@gmail.com>
2019-10-23 13:42:58 +0200

    added slide show to social net and review

--- js/script.js
+++ js/script.js
@@ -1,92 +1,3 @@
-// function hoverInsta() {
-//     document.getElementById('path1-insta').style.fill = '#f7ce9bf3';
-//     document.getElementById('path2-insta').style.fill = '#f7ce9bf3';
-//     document.getElementById('path3-insta').style.fill = '#f7ce9bf3';
-// }
-
-// function notHoverInsta() {
-//     document.getElementById('path1-insta').style.fill = '#ffff';
-//     document.getElementById('path2-insta').style.fill = '#ffff';
-//     document.getElementById('path3-insta').style.fill = '#ffff';
-// }
-
-// function hoverInstaButtom() {
-//     document.getElementById('path1-insta-buttom').style.fill = '#f7ce9bf3';
-//     document.getElementById('path2-insta-buttom').style.fill = '#f7ce9bf3';
-//     document.getElementById('path3-insta-buttom').style.fill = '#f7ce9bf3';
-// }
-
-// function notHoverInstaButtom() {
-//     document.getElementById('path1-insta-buttom').style.fill = '#ffff';
-//     document.getElementById('path2-insta-buttom').style.fill = '#ffff';
-//     document.getElementById('path3-insta-buttom').style.fill = '#ffff';
-// }
-
-// function hoverArrowButton() {
-//     document.getElementById('arrow-down').style.fill = '#B38A58';
-// }
-
-// function notHoverArrowButton() {
-//     document.getElementById('arrow-down').style.fill = 'none';
-// }
-
-function clickRomb(nr) {
-  for (let i = 1; i <= 3; i++) {
-    if (i == nr) {
-      document.getElementsByClassName("romb" + i)[0].style.background = '#b38a58';
-    }
-    else {
-      document.getElementsByClassName("romb" + i)[0].style.background = '#ffff';
-    }
-  }
-}
-
-// function hoverInstaFollowButton() {
-//     document.getElementById('insta-follow-button1').style.fill = '#000000';
-//     document.getElementById('insta-follow-button2').style.fill = '#000000';
-//     document.getElementById('insta-follow-button3').style.fill = '#000000';
-
-// }
-
-// function notHoverInstaFollowButton() {
-//     document.getElementById('insta-follow-button1').style.fill = '#ffff';
-//     document.getElementById('insta-follow-button2').style.fill = '#ffff';
-//     document.getElementById('insta-follow-button3').style.fill = '#ffff';
-
-// }
-
-// function hoverSideArrowButton(side) {
-//     document.getElementById('Path-' + side).style.stroke = '#b38a58';
-// }
-
-// function notHoverSideArrowButton(side) {
-//     document.getElementById('Path-' + side).style.stroke = '#ffff';
-// }
-
-// function hoverArrowUpButton() {
-//     document.getElementById("arrow-up").style.stroke = '#b38a58';
-//     document.getElementById("arrow-up1").style.stroke = '#b38a58';
-
-// }
-
-// function notHoverArrowUpButton() {
-//     document.getElementById("arrow-up").style.stroke = '#ffff';
-//     document.getElementById("arrow-up1").style.stroke = '#ffff';
-
-// }
-
-window.addEventListener('scroll', function () {
-  let scroll = window.scrollY;
-  let vw = document.documentElement.clientWidth;
-  let bgimg = document.querySelector('.mask');
-
-  scroll = scroll / (0.6172 * vw);
-  scroll *= 100;
-  if (scroll < 80) {
-    bgimg.style.backgroundPosition = "50% " + (scroll + 20) + "%";
-  }
-
-});
 
 function moveToFirsBodySection() {
   document.querySelector('.first-body-section').scrollIntoView({
@@ -122,7 +33,9 @@ $(document).ready(function () {
   $('.the-button, #navbarResponsive a').click(function () {
     $('.the-button svg').toggleClass('d-none')
     $('.navbar').toggleClass('dark-bg');
-    $('#navbarResponsive').fadeToggle('slow')
+    $('#navbarResponsive').toggle('fast')
     $('body').toggleClass('overflow-hidden');
   });
 });
+
+
