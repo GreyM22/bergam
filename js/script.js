20a3a56edd3e971a71d64512a538063876f25e14
GreyM22 <greimuka@gmail.com>
2019-11-04 14:31:35 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var rellax = new Rellax('.rellax', {
 $(document).ready(function () {
 
 
-
+/* function to open and close the navigation bar  */
   $('.the-button, #navbarResponsive a').click(function () {
     $('.navbar').toggleClass('dark-bg');
     $('.the-button').toggleClass('rotate');
@@ -129,6 +129,77 @@ $(document).ready(function () {
 
 });
 
+/* Fromating the date in the booking form  */
+var date = document.getElementById('date');
+
+function checkValue(str, max) {
+  if (str.charAt(0) !== '0' || str == '00') {
+    var num = parseInt(str);
+    if (isNaN(num) || num <= 0 || num > max) num = 1;
+    str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();
+  };
+  return str;
+};
+
+date.addEventListener('input', function (e) {
+  this.type = 'text';
+  var input = this.value;
+  if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
+  var values = input.split('/').map(function (v) {
+    return v.replace(/\D/g, '')
+  });
+  if (values[0]) values[0] = checkValue(values[0], 12);
+  if (values[1]) values[1] = checkValue(values[1], 31);
+  var output = values.map(function (v, i) {
+    return v.length == 2 && i < 1 ? v + ' / ' : v;
+  });
+  this.value = output.join('').substr(0, 14);
+});
+
+/* to format the time input */
+var time = document.getElementById('time');
+
+time.addEventListener('input', function (e) {
+  this.type = 'text';
+  var input = this.value;
+  if (/\D\:$/.test(input)) input = input.substr(0, input.length - 3);
+  var values = input.split(':').map(function (v) {
+    return v.replace(/\D/g, '')
+  });
+  if (values[0]) values[0] = checkValue(values[0], 23);
+  if (values[1]) values[1] = checkValue(values[1], 60);
+  var output = values.map(function (v, i) {
+    return v.length == 2 && i < 1 ? v + ' : ' : v;
+  });
+  this.value = output.join('').substr(0, 14);
+});
+
+
+/* smooth scroll for the navigation */
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {        // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
 
 
 
@@ -193,75 +264,7 @@ formEmail.addEventListener("submit", function (e) {
 
 
 
-/* Fromating the date in the booking form  */
-var date = document.getElementById('date');
 
-function checkValue(str, max) {
-  if (str.charAt(0) !== '0' || str == '00') {
-    var num = parseInt(str);
-    if (isNaN(num) || num <= 0 || num > max) num = 1;
-    str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();
-  };
-  return str;
-};
-
-date.addEventListener('input', function (e) {
-  this.type = 'text';
-  var input = this.value;
-  if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
-  var values = input.split('/').map(function (v) {
-    return v.replace(/\D/g, '')
-  });
-  if (values[0]) values[0] = checkValue(values[0], 12);
-  if (values[1]) values[1] = checkValue(values[1], 31);
-  var output = values.map(function (v, i) {
-    return v.length == 2 && i < 1 ? v + ' / ' : v;
-  });
-  this.value = output.join('').substr(0, 14);
-});
-
-var time = document.getElementById('time');
-
-time.addEventListener('input', function (e) {
-  this.type = 'text';
-  var input = this.value;
-  if (/\D\:$/.test(input)) input = input.substr(0, input.length - 3);
-  var values = input.split(':').map(function (v) {
-    return v.replace(/\D/g, '')
-  });
-  if (values[0]) values[0] = checkValue(values[0], 23);
-  if (values[1]) values[1] = checkValue(values[1], 60);
-  var output = values.map(function (v, i) {
-    return v.length == 2 && i < 1 ? v + ' : ' : v;
-  });
-  this.value = output.join('').substr(0, 14);
-});
-
-
-/* smooth scroll for the navigation */
-$(document).ready(function () {
-  // Add smooth scrolling to all links
-  $("a").on('click', function (event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {        // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
 
 
 
